2e85dea98156c106c37cb2cd100f0b92d7ab1537
dkrlto0819 <enqn_k@naver.com>
2019-04-29 21:15:32 +0900

    add multer module

--- routes/write.js
+++ routes/write.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var app = express()
 var router = express.Router();
+var mysql = require('mysql')
+var ejs = require('ejs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
