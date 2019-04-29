ff827e6ff9cde98a8257adf7959ad71154ad5395
WonBeomChoi <cwb2310@gmail.com>
2019-04-29 17:53:40 +0900

    edit html

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var aboutRouter = require('./routes/about');
+var contactRouter = require('./routes/write');
+var productRouter = require('./routes/product');
 var app = express();
 
 // view engine setup
@@ -21,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/write', contactRouter);
+app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
