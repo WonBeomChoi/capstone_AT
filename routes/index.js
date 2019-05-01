97344c94af723849d524641ac714f2d069fffee8
WonBeomChoi <cwb2310@gmail.com>
2019-05-01 10:07:04 +0900

    index mod

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'dnjs2310!@',
+  port     : 3306,
+  database : 'capstone'
+});
+connection.connect();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var query = connection.query('select * from 게시판', function(err,rows){
+    if(err) console.log(err)
+    console.log('rows:'+ rows);
+    res.render('index',{rows:rows});
+  })
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
--- routes/write.js
+++ routes/write.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('write', { title: 'Express' });
+  res.render('write');
 });
 
 module.exports = router;
