1acbd8ebbd5cb1dea036652301f135e12cee52fa
WonBeomChoi <cwb2310@gmail.com>
2019-05-07 14:57:25 +0900

    db con1

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/:게시글번호', function(req, res, next) {
+  var board_no = req.params.게시글번호;
   res.render('product', { title: 'Express' });
 });
 
--- routes/write.js
+++ routes/write.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('write');
+  console.log("asdf");
+  res.render('write',{title:"으아아아아아"});
+});
+router.post('/', (req,res) => {
+  console.log("asdf");
+  return res.status(200).end();
+  
 });
 
 module.exports = router;
